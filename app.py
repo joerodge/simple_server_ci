b27bff3b93b2ce3c561c51f27f4895e80379ad2d
Joe Rodgers <joerodgers89@outlook.com>
2023-12-13 17:11:51 +0000

    Add names arg on get

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def get_home():
-    return "Hello, world!"
+    name = request.args.get('name')
+    return f"Hello, {name}!"
 
 if __name__ == '__main__':
     app.run(
